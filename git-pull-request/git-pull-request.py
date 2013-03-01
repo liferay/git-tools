0936bfb71dba215fb367f011b3257c5ca433c055
Minhchau Dang <minhchau.dang@liferay.com>
2013-03-01 09:01:06 -0500

    May not be a unicode string, so use isdigit instead of isnumeric

--- git-pull-request/git-pull-request.py
+++ git-pull-request/git-pull-request.py
@@ -779,7 +779,7 @@ def get_user_email(github_user_info):
 		if email != None and email.endswith('@liferay.com'):
 			email = email[:-12]
 
-			if email.isnumeric():
+			if email.isdigit():
 				email = None
 		else:
 			email = None
